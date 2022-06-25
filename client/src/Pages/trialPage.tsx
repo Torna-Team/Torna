df3025d63d4182b2e705c840de99313db500409c
JoanMarc <joanmarcdomenech@gmail.com>
2022-06-25 18:20:01 +0200

    Components added, and trial page

---
+++ client/src/Pages/trialPage.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import { useState } from 'react';
+import { Stage, Layer } from 'react-konva';
+import Rectangle from '../components/TransformedRectangle';
+
+const elements = [
+  {
+    fill: 'green',
+    x: 100,
+    y: 100,
+    id: '1a',
+    type: 'Circle',
+    width: 100,
+    height: 100,
+    numPoints: 4,
+    innerRadius: 10,
+    outerRadius: 20,
+    points: [0, 0, 100, 0, 50, 100],
+    closed: true,
+    pointerLength: 6,
+    pointerWidth: 2,
+    stroke: 'green',
+  },
+  {
+    fill: 'blue',
+    x: 100,
+    y: 100,
+    id: '2b',
+    type: 'Rect',
+    width: 100,
+    height: 100,
+    numPoints: 5,
+    innerRadius: 10,
+    outerRadius: 20,
+    points: [50, 150, 150, 50],
+    pointerLength: 2,
+    pointerWidth: 8,
+    stroke: 'blue',
+  },
+  {
+    fill: 'red',
+    x: 100,
+    y: 100,
+    id: '3c',
+    type: 'Rect',
+    width: 50,
+    height: 50,
+    numPoints: 6,
+    innerRadius: 10,
+    outerRadius: 20,
+    pointerLength: 9,
+    pointerWidth: 4,
+    stroke: 'red',
+  },
+];
+
+// type item = {
+//   x: number;
+//   y: number;
+//   width: number;
+//   height: number;
+//   fill: string;
+//   id: string;
+// };
+
+function App() {
+  const [width, setWidth] = useState(800);
+  const [height, setHeight] = useState(300);
+  // const [text, setText] = useState('');
+  const [rectangles, setRectangles] = React.useState(elements);
+  const [selectedId, selectShape] = React.useState<any>(null);
+
+  const handleWheel = (evt: any) => {
+    // evt.preventDefault();
+    console.log(evt.evt.deltaY);
+    if (evt.evt.deltaY > 0) {
+      setHeight(height * 1.05);
+    }
+    if (evt.evt.deltaY < 0) {
+      setHeight(height / 1.05);
+    }
+    // setHeigth(evt.deltaY * 1.5);
+  };
+
+  const checkDeselect = (e: any) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+  };
+
+  return (
+    <div className='App'>
+      <div className='container'>
+        <Stage
+          width={width}
+          height={height}
+          onWheel={handleWheel}
+          onMouseDown={checkDeselect}
+          onTouchStart={checkDeselect}
+        >
+          <Layer>
+            {elements.map((item, i) => {
+              return (
+                <Rectangle
+                  key={i}
+                  shapeProps={item}
+                  isSelected={item.id === selectedId}
+                  onSelect={() => {
+                    selectShape(item.id);
+                  }}
+                  onChange={(newAttrs: any) => {
+                    const items = elements.slice();
+                    items[i] = newAttrs;
+                    setRectangles(items);
+                  }}
+                />
+              );
+            })}
+          </Layer>
+        </Stage>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/TransformedArrow.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Arrow, Transformer } from 'react-konva';
+
+type Props = {
+  shapeProps: any;
+  isSelected: any;
+  onSelect: any;
+  onChange: any;
+};
+
+const Rectangle = ({ shapeProps, isSelected, onSelect, onChange }: Props) => {
+  // console.log('inside the Rectangle component');
+  const shapeRef: any = React.useRef();
+  const trRef: any = React.useRef();
+  React.useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+  return (
+    <React.Fragment>
+      <Arrow
+        onClick={onSelect}
+        onTap={onSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        draggable
+        onDragEnd={(e: any) => {
+          // console.log('inside the Rect Element. onDragend');
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={(e: any) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+
+          // we will reset it back
+          // node.scaleX(1);
+          // node.scaleY(1);
+          onChange({
+            ...shapeProps,
+            x: node.x(),
+            y: node.y(),
+            // set minimal value
+            width: Math.max(5, node.width() * scaleX),
+            height: Math.max(node.height() * scaleY),
+          });
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          boundBoxFunc={(oldBox: any, newBox: any) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Rectangle;
