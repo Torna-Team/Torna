90f572cd60f1d0a0785caab1bd1630d5e24cf7f4
irene <irenecagnoni91@gmail.com>
2022-06-25 14:55:22 +0200

    [FE][UX][working on arrow]

--- client/src/Components/Arrows.tsx
+++ client/src/Components/Arrows.tsx
@@ -6,8 +6,15 @@ function Arrows({ click, setClick }: any) {
   useEffect(() => {
     if (click === 'arrow') {
       const arrow = {
+        type: 'arrow',
         id: Math.random() * 1000,
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2,
       };
+      setArrows((prev: any) => {
+        if (prev) return [...prev, arrow];
+        else return [arrow];
+      });
     }
   });
 
