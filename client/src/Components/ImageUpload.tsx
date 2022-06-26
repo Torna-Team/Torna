09ebfdda455e63d7892805673a53a55efe2e05f6
Ignacio Coll García <nachcoll@gmail.com>
2022-06-26 10:50:15 +0200

    filename upload

--- client/src/Components/ImageUpload.tsx
+++ client/src/Components/ImageUpload.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
-import Canva from './Canva';
+import Canva from './Canva-fileupload';
 
 function ImageUpload() {
   const [images, setImages] = useState<FileList>();
