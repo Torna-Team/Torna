ce768344d4ba78d8b865a091b7a000bc68b9c13c
Xavi Fortuny <fortuny.xavi@gmail.com>
2022-06-30 15:03:29 +0200

    [FE][UX]some css

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -8,109 +8,103 @@ import { login, getUser, User } from '../Services/Server-Client';
 import { LoggingUser } from '../Services/Server-Client';
 import { LoginContext } from '../Utils/Context';
 
-// type LoginProps = {
-// 	setUserId: React.Dispatch<React.SetStateAction<string>>;
-// 	setUserMail: React.Dispatch<React.SetStateAction<string>>;
-// 	setUserName: React.Dispatch<React.SetStateAction<string>>;
-// 	userId: string;
-// 	userMail: string;
-// 	userName: string;
-// };
-
 const Home = () => {
-  const { loggedIn, setLoggedIn } = useContext(LoginContext as any);
-  const navigate = useNavigate();
+	const { loggedIn, setLoggedIn } = useContext(LoginContext as any);
+	const navigate = useNavigate();
 
-  const onSubmitHandler = async (event: React.FormEvent) => {
-    event.preventDefault();
-    const target = event.target as typeof event.target & {
-      email: { value: string };
-      password: { value: string };
-    };
-    const email: string = target.email.value;
-    const password: string = target.password.value;
+	const onSubmitHandler = async (event: React.FormEvent) => {
+		event.preventDefault();
+		const target = event.target as typeof event.target & {
+			email: { value: string };
+			password: { value: string };
+		};
+		const email: string = target.email.value;
+		const password: string = target.password.value;
 
-    const user = {
-      email: email,
-      password: password,
-    };
+		const user = {
+			email: email,
+			password: password,
+		};
 
-    const logged = await login(user as LoggingUser);
+		const logged = await login(user as LoggingUser);
 
-    if ((logged && (logged as any).id) === undefined) {
-      alert('Invalid Email or Password');
-    } else {
-      sessionStorage.setItem('user', JSON.stringify(logged));
-      navigate(`/profile/${(logged as unknown as any).id}`);
-    }
-  };
+		if ((logged && (logged as any).id) === undefined) {
+			alert('Invalid Email or Password');
+		} else {
+			sessionStorage.setItem('user', JSON.stringify(logged));
+			navigate(`/profile/${(logged as unknown as any).id}`);
+		}
+	};
 
-  const checkExistingUser = async (displayName: string, email: string) => {
-    if (displayName && email) {
-      const result = await getUser(displayName, email);
-      console.log(result);
-      return result;
-    }
-  };
+	const checkExistingUser = async (displayName: string, email: string) => {
+		if (displayName && email) {
+			const result = await getUser(displayName, email);
+			console.log(result);
+			return result;
+		}
+	};
 
-  const signInWithGoogle = async () => {
-    try {
-      const provider = new GoogleAuthProvider();
-      const result = await signInWithPopup(auth, provider);
+	const signInWithGoogle = async () => {
+		try {
+			const provider = new GoogleAuthProvider();
+			const result = await signInWithPopup(auth, provider);
 
-      const googleUserName = result.user.displayName;
-      const googleUserMail = result.user.email;
+			const googleUserName = result.user.displayName;
+			const googleUserMail = result.user.email;
 
-      if (googleUserName && googleUserMail) {
-        const user = await checkExistingUser(googleUserName, googleUserMail);
+			if (googleUserName && googleUserMail) {
+				const user = await checkExistingUser(googleUserName, googleUserMail);
 
-        if (user) {
-          sessionStorage.setItem('user', JSON.stringify(user));
-          setLoggedIn(true);
-          navigate(`/profile/${(user as unknown as User).id}`);
-        } else throw new Error();
-      }
-    } catch (error) {
-      alert('Error, try again');
-      console.log(error);
-    }
-  };
+				if (user) {
+					sessionStorage.setItem('user', JSON.stringify(user));
+					setLoggedIn(true);
+					navigate(`/profile/${(user as unknown as User).id}`);
+				} else throw new Error();
+			}
+		} catch (error) {
+			alert('Error, try again');
+			console.log(error);
+		}
+	};
 
-  return (
-    <div className='form-container'>
-      <div>Sign in</div>
-      <form className='login-container' onSubmit={onSubmitHandler}>
-        <label htmlFor='email' className='signInLabel'>
-          Email
-        </label>
-        <input
-          required
-          className='signInInput'
-          type='text'
-          name='email'
-          placeholder='Insert your email'
-        />
-        <label htmlFor='password' className='signInLabel'>
-          Password
-        </label>
-        <input
-          required
-          className='signInInput'
-          type='password'
-          name='password'
-          placeholder='Insert your password'
-        />
-        <button className='signInBtn' onClick={() => setLoggedIn(true)}>
-          Sign In
-        </button>
-      </form>
-      <GoogleButton className='googleBtn' onClick={signInWithGoogle} />
-      <p>
-        Don't have an account?
-        <Link to='/register'>Sign up</Link>
-      </p>
-    </div>
-  );
+	return (
+		<div className='form-container'>
+			<div className='loginWelcome'>
+				<h3>Welcome back</h3>
+				<span>Please enter your details</span>
+			</div>
+			<form className='login-container' onSubmit={onSubmitHandler}>
+				<label htmlFor='email' className='signInLabel'>
+					Email
+				</label>
+				<input
+					required
+					className='signInInput'
+					type='text'
+					name='email'
+					placeholder='Insert your email'
+				/>
+				<label htmlFor='password' className='signInLabel'>
+					Password
+				</label>
+				<input
+					required
+					className='signInInput'
+					type='password'
+					name='password'
+					placeholder='Insert your password'
+				/>
+				<button className='signInBtn' onClick={() => setLoggedIn(true)}>
+					Sign In
+				</button>
+			</form>
+			<GoogleButton className='googleBtn' onClick={signInWithGoogle} />
+			<p>
+				Don't have an account?
+				<Link to='/register'>Sign up</Link>
+			</p>
+		</div>
+	);
 };
 
 export default Home;
