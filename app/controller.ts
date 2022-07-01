891b8f2c9d8da18b180c33d4fce3c9caa3bd51af
Xavi Fortuny <fortuny.xavi@gmail.com>
2022-07-01 08:48:15 +0200

    [BE]bcrypt config

--- app/controller.ts
+++ app/controller.ts
@@ -1,6 +1,6 @@
 import { Response, Request } from 'express';
 import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { v4 as uuidv4 } from 'uuid';
 
 const prisma = new PrismaClient();
