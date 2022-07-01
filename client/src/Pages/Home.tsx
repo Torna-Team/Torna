f594b2e80089a4530f0e78121d4e6d0574364b12
Ignacio Coll García <nachcoll@gmail.com>
2022-07-01 09:25:58 +0200

    [FE][UI][styling of profile and home pages]

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -7,104 +7,113 @@ import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { login, getUser, User } from '../Services/Server-Client';
 import { LoggingUser } from '../Services/Server-Client';
 import { LoginContext } from '../Utils/Context';
+import tornaLogo from '../images/tornalogo.png';
 
 const Home = () => {
-	const { loggedIn, setLoggedIn } = useContext(LoginContext as any);
-	const navigate = useNavigate();
+  const { loggedIn, setLoggedIn } = useContext(LoginContext as any);
+  const navigate = useNavigate();
 
-	const onSubmitHandler = async (event: React.FormEvent) => {
-		event.preventDefault();
-		const target = event.target as typeof event.target & {
-			email: { value: string };
-			password: { value: string };
-		};
-		const email: string = target.email.value;
-		const password: string = target.password.value;
+  const onSubmitHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const target = event.target as typeof event.target & {
+      email: { value: string };
+      password: { value: string };
+    };
+    const email: string = target.email.value;
+    const password: string = target.password.value;
 
-		const user = {
-			email: email,
-			password: password,
-		};
+    const user = {
+      email: email,
+      password: password,
+    };
 
-		const logged = await login(user as LoggingUser);
+    const logged = await login(user as LoggingUser);
 
-		if ((logged && (logged as any).id) === undefined) {
-			alert('Invalid Email or Password');
-		} else {
-			sessionStorage.setItem('user', JSON.stringify(logged));
-			navigate(`/profile/${(logged as unknown as any).id}`);
-		}
-	};
+    if ((logged && (logged as any).id) === undefined) {
+      alert('Invalid Email or Password');
+    } else {
+      sessionStorage.setItem('user', JSON.stringify(logged));
+      navigate(`/profile/${(logged as unknown as any).id}`);
+    }
+  };
 
-	const checkExistingUser = async (displayName: string, email: string) => {
-		if (displayName && email) {
-			const result = await getUser(displayName, email);
-			console.log(result);
-			return result;
-		}
-	};
+  const checkExistingUser = async (displayName: string, email: string) => {
+    if (displayName && email) {
+      const result = await getUser(displayName, email);
+      console.log(result);
+      return result;
+    }
+  };
 
-	const signInWithGoogle = async () => {
-		try {
-			const provider = new GoogleAuthProvider();
-			const result = await signInWithPopup(auth, provider);
+  const signInWithGoogle = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
 
-			const googleUserName = result.user.displayName;
-			const googleUserMail = result.user.email;
+      const googleUserName = result.user.displayName;
+      const googleUserMail = result.user.email;
 
-			if (googleUserName && googleUserMail) {
-				const user = await checkExistingUser(googleUserName, googleUserMail);
+      if (googleUserName && googleUserMail) {
+        const user = await checkExistingUser(googleUserName, googleUserMail);
 
-				if (user) {
-					sessionStorage.setItem('user', JSON.stringify(user));
-					setLoggedIn(true);
-					navigate(`/profile/${(user as unknown as User).id}`);
-				} else throw new Error();
-			}
-		} catch (error) {
-			alert('Error, try again');
-			console.log(error);
-		}
-	};
+        if (user) {
+          sessionStorage.setItem('user', JSON.stringify(user));
+          setLoggedIn(true);
+          navigate(`/profile/${(user as unknown as User).id}`);
+        } else throw new Error();
+      }
+    } catch (error) {
+      alert('Error, try again');
+      console.log(error);
+    }
+  };
 
-	return (
-		<div className='form-container'>
-			<div className='loginWelcome'>
-				<h3>Welcome back</h3>
-				<span>Please enter your details</span>
-			</div>
-			<form className='login-container' onSubmit={onSubmitHandler}>
-				<label htmlFor='email' className='signInLabel'>
-					Email
-				</label>
-				<input
-					required
-					className='signInInput'
-					type='text'
-					name='email'
-					placeholder='Insert your email'
-				/>
-				<label htmlFor='password' className='signInLabel'>
-					Password
-				</label>
-				<input
-					required
-					className='signInInput'
-					type='password'
-					name='password'
-					placeholder='Insert your password'
-				/>
-				<button className='signInBtn' onClick={() => setLoggedIn(true)}>
-					Sign In
-				</button>
-			</form>
-			<GoogleButton className='googleBtn' onClick={signInWithGoogle} />
-			<p>
-				Don't have an account?
-				<Link to='/register'>Sign up</Link>
-			</p>
-		</div>
-	);
+  return (
+    <div className='backgroundContainer'>
+      <div className='form-container'>
+        <div className='loginWelcome'>
+          <img src={tornaLogo} alt='torna logo' />
+          <span>Please enter your details</span>
+        </div>
+        <form className='login-container' onSubmit={onSubmitHandler}>
+          <div className='formInputsContainer'>
+            <label htmlFor='email' className='signInLabel'>
+              Email
+            </label>
+            <input
+              required
+              className='signInInput'
+              type='text'
+              name='email'
+              placeholder='Insert your email'
+            />
+            <label htmlFor='password' className='signInLabel'>
+              Password
+            </label>
+            <input
+              required
+              className='signInInput'
+              type='password'
+              name='password'
+              placeholder='Insert your password'
+            />
+          </div>
+          <div className='signInContainer'>
+            <button className='signInBtn' onClick={() => setLoggedIn(true)}>
+              Sign In
+            </button>
+          </div>
+        </form>
+        <GoogleButton className='googleBtn' onClick={signInWithGoogle} />
+        <p>
+          Don't have an account?
+          <span className='singUpLink'>
+            <Link to='/register'>Sign up</Link>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- client/src/Pages/Profile.tsx
+++ client/src/Pages/Profile.tsx
@@ -4,64 +4,65 @@ import { LoginContext } from '../Utils/Context';
 import { useNavigate, useParams } from 'react-router-dom';
 import '../Styles/Profile.css';
 import { createAlbum } from '../Services/Server-Client';
+import Album from '../Components/Album/Album';
 
 function Profile() {
-	const navigate = useNavigate();
-	// let { id } = useParams();
-	const { loggedIn, setLoggedIn } = useContext(LoginContext as any);
-	const [user, setUser] = useState<any>();
+  const navigate = useNavigate();
+  // let { id } = useParams();
+  const { loggedIn, setLoggedIn } = useContext(LoginContext as any);
+  const [user, setUser] = useState<any>();
 
-	useEffect(() => {
-		if (sessionStorage && sessionStorage.getItem('user')) {
-			const res = JSON.parse(sessionStorage.getItem('user') as string);
-			setUser(res);
-			setLoggedIn(true);
-		}
-	}, [loggedIn]);
+  useEffect(() => {
+    if (sessionStorage && sessionStorage.getItem('user')) {
+      const res = JSON.parse(sessionStorage.getItem('user') as string);
+      setUser(res);
+      setLoggedIn(true);
+    }
+  }, [loggedIn]);
 
-	const editAlbum = (album: any) => {
-		navigate(`/album/${album.id}/edit`);
-	};
+  const editAlbum = (album: any) => {
+    navigate(`/album/${album.id}/edit`);
+  };
 
-	const createNewAlbum = async (user: any) => {
-		const newAlbum = await createAlbum(user);
-		console.log(newAlbum);
-		navigate(`/album/${newAlbum.id}/edit`);
-	};
+  const createNewAlbum = async (user: any) => {
+    const newAlbum = await createAlbum(user);
+    console.log(newAlbum);
+    navigate(`/album/${newAlbum.id}/edit`);
+  };
 
-	return (
-		<div className='ProfileMainContainer'>
-			<Navbar user={user} />
-			<div className='albumsContainer'>
-				{loggedIn ? (
-					<>
-						<div className='albumList'>
-							{user && user.albums && user.albums.length !== 0 ? (
-								user.albums.map((el: any) => {
-									return (
-										<div
-											className='albumFrontPage'
-											onClick={() => editAlbum(el)}
-										>
-											<h4 className='albumTitle'>{el.title}</h4>
-											<img className='albumFrontImage' src={el.frontPage} />
-										</div>
-									);
-								})
-							) : (
-								<p>You don't have albums yet</p>
-							)}
-						</div>
-					</>
-				) : (
-					<h3>Log in to see your albums or to create a new one </h3>
-				)}
-			</div>
-			{loggedIn && (
-				<button onClick={() => createNewAlbum(user)}>Create a new Album</button>
-			)}
-		</div>
-	);
+  return (
+    <div className='ProfileMainContainer'>
+      <Navbar user={user} />
+      <div className='albumsContainer'>
+        {loggedIn ? (
+          <>
+            <div className='albumList'>
+              {user && user.albums && user.albums.length !== 0 ? (
+                user.albums.map((el: any) => {
+                  return (
+                    <Album
+                      className='singleAlbum'
+                      element={el}
+                      editAlbum={editAlbum}
+                    />
+                  );
+                })
+              ) : (
+                <p>You don't have albums yet</p>
+              )}
+            </div>
+          </>
+        ) : (
+          <h3>Log in to see your albums or to create a new one </h3>
+        )}
+      </div>
+      {loggedIn && (
+        <button className='newAlbumButton' onClick={() => createNewAlbum(user)}>
+          Create a new Album
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default Profile;
