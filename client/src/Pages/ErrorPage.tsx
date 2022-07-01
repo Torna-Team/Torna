e8014de272df8586caec9f30db0dd1487956fe60
Xavi Fortuny <fortuny.xavi@gmail.com>
2022-07-01 11:07:52 +0200

    [FE][UI]error-page-css

--- client/src/Pages/ErrorPage.tsx
+++ client/src/Pages/ErrorPage.tsx
@@ -1,10 +1,18 @@
 import '../Styles/ErrorPage.css';
+import { Link } from 'react-router-dom';
 
 function ErrorPage() {
 	return (
-		<div className='errorPage'>
-			<h1>Ooops...</h1>
-			<h2>Something went wrong</h2>
+		<div className='errorMainPage'>
+			<div className='errorNumberPage'>
+				<h1 className='num404'>404</h1>
+			</div>
+			<div className='errorPage'>
+				<h2>Something went wrong. The page you requested could not be found</h2>
+				<Link to='/'>
+					<button>Go back home</button>
+				</Link>
+			</div>
 		</div>
 	);
 }
--- client/src/Services/Server-Client.ts
+++ client/src/Services/Server-Client.ts
@@ -1,121 +1,121 @@
 import tornaLogo from '../images/tornalogo.png';
 
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://torna-backend.herokuapp.com';
 
 export interface User {
-  id: string;
-  firstName: string;
-  lastName?: string;
-  password?: string;
-  albums?: any[];
-  email: string;
+	id: string;
+	firstName: string;
+	lastName?: string;
+	password?: string;
+	albums?: any[];
+	email: string;
 }
 
 export interface LoggingUser {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 
 export const getUser = async (displayName: string, email: string) => {
-  try {
-    const result = await fetch(`${BASE_URL}/user/`, {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({
-        firstName: displayName,
-        lastName: '',
-        email: email,
-      }),
-    });
-    const json = await result.json();
-    return json;
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const result = await fetch(`${BASE_URL}/user/`, {
+			method: 'POST',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify({
+				firstName: displayName,
+				lastName: '',
+				email: email,
+			}),
+		});
+		const json = await result.json();
+		return json;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const login = async (user: LoggingUser) => {
-  try {
-    const result = await fetch(`${BASE_URL}/login`, {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify(user),
-    });
-    const res = await result.json();
-    return res as User;
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const result = await fetch(`${BASE_URL}/login`, {
+			method: 'POST',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(user),
+		});
+		const res = await result.json();
+		return res as User;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const register = async (newuser: User) => {
-  try {
-    const result = await fetch(`${BASE_URL}/register`, {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify(newuser),
-    });
-    const res = await result.json();
-    return res as User;
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const result = await fetch(`${BASE_URL}/register`, {
+			method: 'POST',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(newuser),
+		});
+		const res = await result.json();
+		return res as User;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 export const getAlbum = async (id: number) => {
-  try {
-    const result = await fetch(`${BASE_URL}/album/${id}`, {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json',
-      },
-    });
-    console.log(result);
-    const res = await result.json();
-    console.log(res);
-    return res as any;
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const result = await fetch(`${BASE_URL}/album/${id}`, {
+			method: 'GET',
+			headers: {
+				'Content-type': 'application/json',
+			},
+		});
+		console.log(result);
+		const res = await result.json();
+		console.log(res);
+		return res as any;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 export const saveAlbum = async (album: any) => {
-  try {
-    console.log(album);
-    const result = await fetch(`${BASE_URL}/album/${album.id}`, {
-      method: 'PUT',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify(album),
-    });
-    console.log(result);
-    const res = await result.json();
-    console.log(res);
-    return res as any;
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		console.log(album);
+		const result = await fetch(`${BASE_URL}/album/${album.id}`, {
+			method: 'PUT',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(album),
+		});
+		console.log(result);
+		const res = await result.json();
+		console.log(res);
+		return res as any;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 export const createAlbum = async (user: any) => {
-  try {
-    console.log(user);
-    const album = {
-      title: 'new album',
-      template: '',
-      background: 'rgba(255, 255, 255)',
-      frontPage: tornaLogo,
-      author: user.id,
-    };
-    const result = await fetch(`${BASE_URL}/album/`, {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify(album),
-    });
-    console.log(result);
-    const res = await result.json();
-    console.log(res);
-    return res as any;
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		console.log(user);
+		const album = {
+			title: 'new album',
+			template: '',
+			background: 'rgba(255, 255, 255)',
+			frontPage: tornaLogo,
+			author: user.id,
+		};
+		const result = await fetch(`${BASE_URL}/album/`, {
+			method: 'POST',
+			headers: { 'Content-type': 'application/json' },
+			body: JSON.stringify(album),
+		});
+		console.log(result);
+		const res = await result.json();
+		console.log(res);
+		return res as any;
+	} catch (error) {
+		console.error(error);
+	}
 };
