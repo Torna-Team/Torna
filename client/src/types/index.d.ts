6fdb7186f60c992e943887870aab55f7f2d798df
Xavi Fortuny <fortuny.xavi@gmail.com>
2022-07-02 12:53:08 +0200

    [FE]Just a typescript navbar

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -1,25 +1,25 @@
 export {};
 
 declare global {
-  interface Window {
-    gifler: any; // 👈️ turn off type checking
-  }
+	interface Window {
+		gifler: any; // 👈️ turn off type checking
+	}
 
-  type User = {
-    id: number;
-    firstName: string;
-    lastName: string;
-    password: string;
-    email: string;
-    albums: Album[];
-  };
+	type User = {
+		id: number;
+		firstName: string;
+		lastName: string;
+		password: string;
+		email: string;
+		albums: Album[];
+	};
 
-  type Album = {
-    id: number;
-    title: string;
-    template: string;
-    authorId: number;
-    background: string;
-    frontPage: string;
-  };
-
+	type Album = {
+		id: number;
+		title: string;
+		template: string;
+		authorId: number;
+		background: string;
+		frontPage: string;
+	};
+}
